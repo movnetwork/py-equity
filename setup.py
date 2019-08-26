63a5695aeb8160f5a8080262364cb4a74f005f67
meherett <meherett@zoho.com>
2019-08-26 10:03:19 -0400

    init py-equity

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as readme:
+    long_description = readme.read()
+
+with open("requirements.txt", "r") as _requirements:
+    requirements = list(map(str.strip, _requirements.read().split("\n")))[:-1]
+
+setup(
+    name="py-equity",
+    version='0.1.0',
+    description='The implementation of Hierarchical Deterministic (HD) wallets generator for Bytom blockchain',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    license='MIT',
+    author='Meheret Tesfaye',
+    author_email='meherett@zoho.com',
+    url='https://github.com/mehetett/py-equity',
+    packages=find_packages(),
+    python_requires=">=3.5,<4",
+    install_requires=requirements,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7"
+    ],
+)
