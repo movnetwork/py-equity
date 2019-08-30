cfad864dded088aea476418fd60f7eea5cc61ff6
meherett <meherett@zoho.com>
2019-08-30 05:32:38 -0400

    added value error exception

--- equity/exceptions.py
+++ equity/exceptions.py
@@ -50,3 +50,12 @@ class InvalidURL(Exception):
 
     def __str__(self):
         return "%s" % self.error_message
+
+
+class ValueError(Exception):
+
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+    def __str__(self):
+        return "%s" % self.error_message
