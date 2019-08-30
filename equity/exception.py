e58dc4348b4cada1689454b4884d29dde7c0f99d
meherett <meherett@zoho.com>
2019-08-30 00:02:46 -0400

    EquityConnectionError function added

--- equity/exception.py
+++ equity/exception.py
@@ -23,3 +23,12 @@ class EquityClientError(Exception):
             return "(%s) %s" % (self.status_code, self.error_message)
         else:
             return self.error_message
+
+
+class EquityConnectionError(Exception):
+
+    def __init__(self, error_message):
+        self.error_message = error_message
+
+    def __str__(self):
+        return "%s" % self.error_message
