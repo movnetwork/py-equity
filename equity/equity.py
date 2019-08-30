abb5717fe9b14e693503c23bb53ce7c8348d0a2f
meherett <meherett@zoho.com>
2019-08-30 12:42:04 -0400

    compile_file and compile_source upgraded

--- equity/equity.py
+++ equity/equity.py
@@ -2,28 +2,97 @@
 # coding=utf-8
 
 # IMPORT ALL PACKAGES
-from equity import *
-from equity.utils import post_body
+from equity.utils import file_reader, file_writer, get_instance_name
+from equity.exceptions import ConnectionError, Timeout, InvalidURL, ValueError
+from equity.rpc import RPC
+import requests
+
+# Info
+__NAME__ = "PY-Equity"
+__VERSION__ = "0.1.0"
+__AUTHOR__ = "Meheret Tesfaye"
+__LICENSE__ = "AGPLv3+"
+
+# Default bytom url
+DEFAULT_URL = "http://localhost:9888"
+TIMEOUT = 1
 
 
-class RPC(object):
+class Equity(object):
 
-    def __init__(self, api):
-        self.api = api
+    def __init__(self, url=None, api_key=None, timeout=None):
+        self.url = DEFAULT_URL if not url else url
+        if api_key and isinstance(api_key, str):
+            self.api_key = str(api_key).split(":")
+        else:
+            self.api_key = str()
+        if timeout and isinstance(timeout, int):
+            self.timeout = int(timeout)
+        else:
+            self.timeout = TIMEOUT
+        self._response = dict()
+        self._check_url(self.url)
 
-    def get_base_path(self):
-        return self.api.base_path
+    def _check_url(self, url):
+        try:
+            return requests.get(url, timeout=self.timeout)
+        except requests.exceptions.InvalidURL:
+            raise InvalidURL("Wrong url, Please check your's url(%s)!" % url)
+        except requests.exceptions.ConnectionError:
+            raise ConnectionError("Please check your's internet connection or your's url(%s)!" % url)
+        except requests.exceptions.Timeout:
+            raise Timeout("Timeout error url(%s)!" % url)
+        except Exception:
+            raise ConnectionError("Something is wrong url(%s)!" % url)
 
-    def full_url(self, path):
-        return "%s%s" % (self.api.url, path)
+    def is_working(self, url=None):
+        try:
+            if url and isinstance(url, str):
+                return True if requests.get(url, timeout=1).status_code == 200 else False
+            else:
+                return True if requests.get(self.url, timeout=1).status_code == 200 else False
+        except Exception:
+            return False
 
-    def prepare(self, path, params):
-        return self.full_url(path), post_body(params), dict()
+    def compile_file(self, file_path, args=None):
+        if args is not None:
+            if not isinstance(args, list):
+                raise ValueError("Invalid instance of args! please use list instance.")
+            else:
+                _args = list()
+                for arg in args:
+                    instance_name = get_instance_name(arg)
+                    _args.append({instance_name: arg})
+        else:
+            _args = list()
+        equity_source = file_reader(file_path=file_path)
+        _requests = dict(contract=equity_source, args=_args)
+        rpc = RPC(self.url, self.api_key)
+        compile_url = rpc.compile_url()
+        _response = rpc.post(compile_url, _requests)
+        self._response = _response
+        return self._response
 
-    def create(self, url, body=None, headers=None):
-        headers = headers or dict()
-        if 'User-Agent' not in headers:
-            headers.update({"User-Agent": "%s Python Client - %s" % (self.api.api_name, self.api.version)})
-        if 'Content-Type' not in headers:
-            headers.update({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"})
-        return requests.post(url, data=body, headers=headers, auth=self.api.auth)
+    def compile_source(self, equity_source, args=None):
+        if args is not None:
+            if not isinstance(args, list):
+                raise ValueError("Invalid instance of args! please use list instance.")
+            else:
+                _args = list()
+                for arg in args:
+                    instance_name = get_instance_name(arg)
+                    _args.append({instance_name: arg})
+        else:
+            _args = list()
+        if not isinstance(equity_source, str):
+            raise ValueError("Invalid instance of equity_source! please use only string(str) instance.")
+        _requests = dict(contract=equity_source, args=_args)
+        rpc = RPC(self.url, self.api_key)
+        compile_url = rpc.compile_url()
+        _response = rpc.post(compile_url, _requests)
+        self._response = _response
+        return self._response
+
+    def save(self, file_path):
+        return file_writer(file_path, self._response)
+
