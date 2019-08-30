a8b85f1b566b8c45161383d9bee5902a843d8397
meherett <meherett@zoho.com>
2019-08-30 13:22:55 -0400

    Update packages

--- equity/__init__.py
+++ equity/__init__.py
@@ -2,15 +2,19 @@
 # coding=utf-8
 
 from collections import namedtuple
+from .equity import Equity
 
 import json
 import requests
 import six
+import os
 
 
 __all__ = [
+    "os",
     "json",
     "requests",
     "namedtuple",
-    "six"
+    "six",
+    "Equity"
 ]
