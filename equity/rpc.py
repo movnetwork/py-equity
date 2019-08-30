f85d19643ce85103007fb3c2b707cd5019ee47b1
meherett <meherett@zoho.com>
2019-08-30 05:29:34 -0400

    Upgraded rpc.py

--- equity/rpc.py
+++ equity/rpc.py
@@ -3,34 +3,41 @@
 
 # IMPORT ALL PACKAGES
 from equity import *
+from equity.utils import post_body
+
+
+API_NAME = "Bytom"
+VERSION = "1.0.9"
 
 
 class RPC(object):
 
-    def __init__(self, api):
-        self.api = api
+    def __init__(self, url, equity_source, auth=None, api_name=None, version=None):
+        self.url = str(url)
+        self.equity_source = str(equity_source)
+        self.auth = tuple() if auth is None else tuple(auth)
+        self.api_name = API_NAME if not api_name else api_name
+        self.version = VERSION if not version else version
 
-    def get_base_path(self):
-        return self.api.base_path
+    def compile_url(self, path):
+        if self.url.endswith("/"):
+            return "%s%s" % (self.url, "compile")
+        else:
+            return "%s%s" % (self.url, "/compile")
 
-    def full_url(self, path):
-        return "%s%s" % (self.api.url, path)
-
-    def prepare(self, path, params):
-        return self.full_url(path), post_body(params), dict()
-
-    def create(self, url, body=None, headers=None):
+    def check_header(self, headers):
         headers = headers or dict()
         if 'User-Agent' not in headers:
-            headers.update({"User-Agent": "%s Python Client - %s" % (self.api.api_name, self.api.version)})
+            headers.update({"User-Agent": "%s Python Client - %s" % (self.api_name, self.version)})
         if 'Content-Type' not in headers:
             headers.update({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"})
-        return requests.post(url, data=body, headers=headers, auth=self.api.auth)
+        return headers
+
+    def post(self, url, body=None, headers=None):
+        body = post_body(body)
+        headers = self.check_header(headers)
+        return requests.post(url, data=body, headers=headers, auth=self.auth)
 
     def get(self, url, headers=None):
-        headers = headers or dict()
-        if 'User-Agent' not in headers:
-            headers.update({"User-Agent": "%s Python Client - %s" % (self.api.api_name, self.api.version)})
-        if 'Content-Type' not in headers:
-            headers.update({"Content-Type": "application/x-www-form-urlencoded; charset=utf-8"})
-        return requests.get(url, headers=headers, auth=self.api.auth)
+        headers = self.check_header(headers)
+        return requests.get(url, headers=headers, auth=self.auth)
